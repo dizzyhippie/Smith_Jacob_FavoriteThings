7c647bb04e723035f2dda37f88873d1211713c19
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-15 14:26:54 -0400

    Full Functionality with modal

--- js/main.js
+++ js/main.js
@@ -7,12 +7,33 @@ const   ccrButton = document.querySelector("#first"),
         coffeeButton = document.querySelector("#second"),
         bopButton = document.querySelector("#third"),
         buttons = document.querySelectorAll("button"),
-        thingName = document.querySelector(".itemTitle"),
-        thingFact = document.querySelector(".itemFact"),
-        thingDesc = document.querySelector(".itemDesc"),
+        thingName = document.querySelector(".fav-name"),
+        thingFact = document.querySelector(".fav-fact"),
+        thingDesc = document.querySelector(".fav-desc"),
         container = document.querySelector(".favoriteContainer"),
         template = document.querySelector("#itemTemplate").content;
 
+
+let favorites = {
+        "CCR": {
+            "name":"Creedence Clearwater Revival",
+            "description":"The greatest band to ever do it.",
+            "fact":"While their musical style is Louisiana Swamp Rock, the band members are from California.",
+            "pic":"CCR.png"
+        },
+        "Coffee": {
+            "name":"Iced Shaken Espresso",
+            "description":"A dangerous amount of caffeine.",
+            "fact":"It is expensive.",
+            "pic":"Coffee.png"
+        },
+        "BOP": {
+            "name":"Birds of Paradise",
+            "description":"The favourite of my plant collection, a beautiful addition to my living room.",
+            "fact":"The flower resembles a bird, and symbolizes ultimate freedom.",
+            "pic":"BOP.png"
+        }
+};
     
 function getThings(){
         
@@ -20,39 +41,37 @@ function getThings(){
         .then(res => res.json())//unpack the API response
         .then(data => {
             console.table(data);//Send to console
-            buildModal(data);
+            //buildModal(data);
             })
         .catch(error => console.error(error));//find and report any errors into the console window
     }
         // get data on page load
         getThings();
           
-function buildModal(info){
+//function buildModal(info){
     //Take the information from the data object
-    const favorites = Object.keys(info);
+    //const favorites = Object.keys(info);
 
-    favorites.forEach(favorite => {
+    //favorites.forEach(favorite => {
         //Make a copy of the template container
-        let modal = template.cloneNode(true);
+        //let modal = template.cloneNode(true);
         //Entrance point of the template content
-        let information = modal.firstElementChild.children;
+        //let information = modal.firstElementChild.children;
 
-        //Load the images to act as Icon/Buttons
-        information[0].querySelector("img").src =`images/${info[favorite].pic}`;
-
-        container.appendChild(modal);
-    })
-}
+        //container.appendChild(modal);
+    //})
+//}
 
 function showData(){
-   let key = this.dataset.key;
-   debugger;
+    let key = this.dataset.key;
+    let picture = document.querySelector(".fav-img");
+
+    picture.querySelector("img").src = `images/${favorites[key].pic}`;
+    thingName.textContent = favorites[key].name;
+    thingFact.textContent = favorites[key].fact;
+    thingDesc.textContent = favorites[key].description;
 }
 
-
-//ccrButton.addEventListener("click", getThings);
 buttons.forEach(button => button.addEventListener("click", showData));
-//buttons.addEventListener("click", showData);
-
 
 })()
