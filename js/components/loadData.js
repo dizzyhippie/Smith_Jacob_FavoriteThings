20d079b616ecc08b57c0373e623815eed07460ad
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-14 20:49:02 -0400

    Trying to get information retrieval working.

--- js/components/loadData.js
+++ js/components/loadData.js
@@ -5,10 +5,10 @@ function getData(){
     .then(data => {
         console.table(data);//Send to console
 
-        //function to create our dynamic content
     })
     .catch(error => console.error(error));//find and report any errors
 }
+
 // get data on page load, and export to main.js file
 getData();
 
