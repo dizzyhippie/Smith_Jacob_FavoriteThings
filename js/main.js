98b12484edd520007e986e228cb9247b57a9d631
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-15 17:52:02 -0400

    Data Fix complete

--- js/main.js
+++ js/main.js
@@ -12,26 +12,7 @@ const   buttons = document.querySelectorAll(".nav-buttons"),
         closeButton = document.querySelector(".close");
 
 
-let favorites = {
-        "CCR": {
-            "name":"Creedence Clearwater Revival",
-            "description":"The greatest band to ever do it.",
-            "fact":"Fun Fact: While their musical style is Louisiana Swamp Rock, the band members are from California.",
-            "pic":"CCR.png"
-        },
-        "Coffee": {
-            "name":"Iced Shaken Espresso",
-            "description":"A dangerous amount of caffeine.",
-            "fact":"Fun Fact: It is expensive.",
-            "pic":"Coffee.png"
-        },
-        "BOP": {
-            "name":"Birds of Paradise",
-            "description":"The favourite of my plant collection, a beautiful addition to my living room.",
-            "fact":"Fun Fact: The flower resembles a bird, and symbolizes ultimate freedom.",
-            "pic":"BOP.png"
-        }
-};
+let favorites = {};
     
 function getThings(){
         
@@ -39,26 +20,12 @@ function getThings(){
         .then(res => res.json())//unpack the API response
         .then(data => {
             console.table(data);//Send to console
-            //buildModal(data);
+            favorites = data;
             })
         .catch(error => console.error(error));//find and report any errors into the console window
     }
         // get data on page load
         getThings();
-          
-//function buildModal(info){
-    //Take the information from the data object
-    //const favorites = Object.keys(info);
-
-    //favorites.forEach(favorite => {
-        //Make a copy of the template container
-        //let modal = template.cloneNode(true);
-        //Entrance point of the template content
-        //let information = modal.firstElementChild.children;
-
-        //container.appendChild(modal);
-    //})
-//}
 
 function showData(){
     let modal = document.querySelector(".modal-panel");
